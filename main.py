8072ba2eb895f0cf67bd40caf5d8bc8075f7ef6f
antivikk <antivikk66@gmail.com>
2026-05-03 13:39:19 -0400

    Calculater

---
+++ main.py
@@ -0,0 +1,15 @@
+a = int(input("Enter the first number"))
+b = int(input("Enter the second number"))
+c = input("What is the Opperation?(+,-,*,/)")
+
+if c == "+":
+    print(a + b)
+
+elif c == "-":
+    print(a - b)
+
+elif c == "*":
+    print(a * b)
+
+elif c == "/":
+    print(a / b)
